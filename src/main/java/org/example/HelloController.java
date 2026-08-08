0db4212db6855f7fcaf09711882c2d94bdd89572
Andrey_Vorobev <andrey.vorobev.aqa@gmail.com>
2026-08-08 20:21:34 +0300

    Revert "# Первый блок: перенаправляем весь HTTP-трафик на HTTPS"
    This reverts commit 113470db312c105b15f794096cacdf0e435ec285.

--- src/main/java/org/example/HelloController.java
+++ src/main/java/org/example/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello, World! 🚀 Deployed via GitHub Actions!";
+        return "Hello, World! 🚀";
     }
 
     @GetMapping("/ping")
